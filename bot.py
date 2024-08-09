b9cff8fe0433c28cc05d6011f8e44a4ec7ad7d2f
Sriharsh I <149700868+AstraZeneca365@users.noreply.github.com>
2024-08-09 13:01:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import mysql.connector
-
+import time
 
 def connect_to_database():
     db = {
         'host': 'localhost',
         'user': 'root',
-        'password': '1234',
+        'password': 'root',
         'database': 'comp_project'
     }
     try:
@@ -63,83 +63,118 @@ def identify_emotion_from_sentence(sentence):
     return None, None
 
 def main():
-    st.markdown("""
-    <style>
-        .title {
-            color: #FF6347; /* Tomato color */
-            font-size: 36px;
-            font-weight: bold;
-        }
-        .header {
-            color: #4682B4; /* SteelBlue color */
-            font-size: 28px;
-        }
-        .description {
-            color: #32CD32; /* LimeGreen color */
-            font-size: 18px;
-        }
-        .song-details {
-            background-color: #F0E68C; /* Khaki color */
-            padding: 10px;
-            border-radius: 5px;
-            box-shadow: 2px 2px 5px rgba(0,0,0,0.1);
-        }
-    </style>
-    """, unsafe_allow_html=True)
 
-    st.markdown('<p class="title">🎵 Song Recommender Chatbot 🎵</p>', unsafe_allow_html=True)
+    fade_style = """
+<style>
+.fade-out {
+    animation: fadeOut 1s forwards;
+}
 
-    st.markdown("""
-    <p class="description">
-    <br> 
-    Describe your mood or enter a sentence that reflects how you're feeling, and we'll recommend some songs that match your mood.
-    </p>
-    """, unsafe_allow_html=True)
+@keyframes fadeOut {
+    0% { opacity: 1; }
+    100% { opacity: 0; }
+}
+</style>
+"""
 
-    connection = connect_to_database()
-    if not connection:
-        st.error("Failed to connect to database.")
-        return
+    st.markdown(fade_style, unsafe_allow_html=True)
+    intro_visible = st.session_state.get('intro_visible', True)
 
-    cursor = connection.cursor()
+    if intro_visible:
+        st.title("Home")
+        st.write("This is Spots. I love to talk to people and recommends you some cool songs based on your mood")
+        if st.button("LETS CHAT!"):
+            st.session_state.intro_visible = False
+            time.sleep(1)
+            st.experimental_rerun()
 
-    with st.form(key='mood_form'):
-        user_input = st.text_input("Describe your mood:", placeholder="e.g., I am feeling really joyful today.")
-        num_songs = st.selectbox("How many songs would you like?", options=range(1, 11), index=4)
-        submit_button = st.form_submit_button("Get Recommendations")
+       
+        st.markdown("[Click here to send us some of your recommendations!](https://www.google.com)")
 
-        if submit_button:
-            if not user_input.strip():
-                st.warning("Please enter something to get recommendations.")
-            else:
-                mood, synonym = identify_emotion_from_sentence(user_input) or (
-                    user_input.capitalize() if user_input.capitalize() in emotions else (None, None))
 
-                if mood:
-                    emotion_id = emotion_ids[emotions.index(mood)]
+    
+    else:
+        st.markdown("""
+        <style>
+            .title {
+                color: #FF6347; /* Tomato color */
+                font-size: 36px;
+                font-weight: bold;
+            }
+            .header {
+                color: #4682B4; /* SteelBlue color */
+                font-size: 28px;
+            }
+            .description {
+                color: #32CD32; /* LimeGreen color */
+                font-size: 18px;
+            }
+            .song-details {
+                background-color: #F0E68C; /* Khaki color */
+                padding: 10px;
+                border-radius: 5px;
+                box-shadow: 2px 2px 5px rgba(0,0,0,0.1);
+            }
+        </style>
+        """, unsafe_allow_html=True)
 
-                    songs = fetch_songs_by_emotion(cursor, emotion_id, limit=num_songs)
+        st.markdown('<p class="title">🎵 Song Recommender Chatbot 🎵</p>', unsafe_allow_html=True)
 
-                    if songs:
-                        display_mood = synonym if synonym else mood
-                        st.markdown(f'<p class="header">Here are some amazing songs for "{display_mood}":</p>',
-                                    unsafe_allow_html=True)
-                        for song in songs:
-                            st.markdown(f"""
-                            <div class="song-details">
-                                <strong>Name:</strong> {song[0]}<br>
-                                <strong>Artist:</strong> {song[1]}<br>
-                                <strong>Genre:</strong> {song[2]}<br>
-                                <strong>Spotify Link:</strong> <a href="{song[3]}">{song[0]}</a>
-                            </div>
-                            """, unsafe_allow_html=True)
-                    else:
-                        st.warning(f"No songs found for '{mood}'.")
+        st.markdown("""
+        <p class="description">
+        <br> 
+        Describe your mood or enter a sentence that reflects how you're feeling, and we'll recommend some songs that match your mood.
+        </p>
+        """, unsafe_allow_html=True)
+
+        connection = connect_to_database()
+        if not connection:
+            st.error("Failed to connect to database.")
+            return
+
+        cursor = connection.cursor()
+
+        with st.form(key='mood_form'):
+            user_input = st.text_input("Describe your mood:", placeholder="e.g., I am feeling really joyful today.")
+            num_songs = st.selectbox("How many songs would you like?", options=range(1, 11), index=4)
+            submit_button = st.form_submit_button("Get Recommendations")
+
+            if submit_button:
+                if not user_input.strip():
+                    st.warning("Please enter something to get recommendations.")
                 else:
-                    st.error("Sorry, I don't understand that mood. Please try again.")
+                    mood, synonym = identify_emotion_from_sentence(user_input) or (
+                        user_input.capitalize() if user_input.capitalize() in emotions else (None, None))
 
-    cursor.close()
-    connection.close()
+                    if mood:
+                        emotion_id = emotion_ids[emotions.index(mood)]
+
+                        songs = fetch_songs_by_emotion(cursor, emotion_id, limit=num_songs)
+
+                        if songs:
+                            display_mood = synonym if synonym else mood
+                            st.markdown(f'<p class="header">Here are some amazing songs for "{display_mood}":</p>',
+                                        unsafe_allow_html=True)
+                            for song in songs:
+                                st.markdown(f"""
+                                <div class="song-details">
+                                    <strong>Name:</strong> {song[0]}<br>
+                                    <strong>Artist:</strong> {song[1]}<br>
+                                    <strong>Genre:</strong> {song[2]}<br>
+                                    <strong>Spotify Link:</strong> <a href="{song[3]}">{song[0]}</a>
+                                </div>
+                                """, unsafe_allow_html=True)
+                        else:
+                            st.warning(f"No songs found for '{mood}'.")
+                    else:
+                        st.error("Sorry, I don't understand that mood. Please try again.")
+
+        cursor.close()
+        connection.close()
+
+
+
+    
 
 if __name__ == "__main__":
     main()
