99e213d38687bd70b2588f95ac6cf72bfbd329e1
Sriharsh I <149700868+AstraZeneca365@users.noreply.github.com>
2024-08-11 12:27:07 +0530

    Add files via upload

---
+++ form.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import mysql.connector as mc
+
+def form():
+
+    st.title("Recommend some of your Favourites!!")
+
+    with st.form(key='question_form'):
+
+        name = st.text_input("Enter the name of the song",placeholder="e.g., I Ain't Worried")
+
+        artist = st.text_input("Enter the name of the artist",placeholder="e.g., One Republic")
+
+        emotion_id = st.text_input("""Describe the emotion of your song--- \n 
+Available emotions - [Happy, Energetic, Love, Motivational, Focused, HeartBreak, Angry, Calm, Sad, Chill, Party]     
+         """)
+
+        genre = st.text_input("Enter the genre of your song", placeholder="e.g., Pop    ")
+
+        spotify_link = st.text_input("Paste the spotify link of the song")
+
+        submit_button = st.form_submit_button(label='Submit')
+
+    if submit_button:
+        db = {
+            'host': 'localhost',
+            'user': 'root',
+            'password': '1234',
+            'database': 'comp_project'
+        }
+        connection = mc.connect(**db)
+        cursor = connection.cursor()
+
+        query = f"""
+                    create table if not exists rc_songs(
+                    name varchar(50),
+                    artist varchar(100),
+                    emotion_id char(20),
+                    genre varchar(20),
+                    spotify_link varchar(80));
+                """
+        cursor.execute(query)
+
+        cursor.execute(f"""insert into rc_songs values(\"{name}\", \"{artist}\", \"{emotion_id}\", \"{genre}\", \"{spotify_link}\") """)
+        connection.commit()
